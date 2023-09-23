914eac9a1548eba2c776077d5f197a242628be31
yury <shubin1978@omnicommdv.ru>
2023-09-23 13:07:19 +0800

    Добавление класса Mark

---
+++ Models/Mark.cs
@@ -0,0 +1,10 @@
+﻿namespace UniversityApp.Models;
+
+public record Mark
+{
+    public string Id { get; init; }
+    public DateTime Date { get; init; }
+    public string Subject { get; init; }
+    public Teacher Te { get; init; }
+    public int Rating { get; init; }
+}
