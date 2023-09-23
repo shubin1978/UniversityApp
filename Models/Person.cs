cc3ff1fc7139053898b3426b494acb1b4b9b6ee3
yury <shubin1978@omnicommdv.ru>
2023-09-23 12:56:20 +0800

    Добавление класса Person

---
+++ Models/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace UniversityApp.Models;
+
+public record Person
+{
+    public Guid Id{ get; init; }
+    public string LastName { get; init; }
+    public string FirstName { get; init; }
+    public string? Patronymic { get; init; }
+    public string FullName => $"{LastName} {FirstName} {Patronymic ?? ""}";
+    
+    public DateTime DateOfBirth { get; init; }
+
+    public int Age
+    {
+        get
+        {
+            var today = DateTime.Today;
+            var age = today.Year - DateOfBirth.Year;
+            if (DateOfBirth.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+    }
+
+    public Sex Sex { get; init; }
+};
