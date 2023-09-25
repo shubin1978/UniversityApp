0d499af909465f0c3b923c0ee184f586fa7e0fce
yury <shubin1978@omnicommdv.ru>
2023-09-25 10:46:44 +0800

    Добавлен интерфейс IContext

---
+++ DataAccessLevel/IContext.cs
@@ -0,0 +1,15 @@
+﻿using UniversityApp.Models;
+
+namespace UniversityApp.DataAccessLevel;
+
+public interface IContext
+{
+    public List<Teacher> Teachers { get; set; }
+    public List<Student> Students { get; set; }
+
+    public void ImportTeachers();
+    public void ExportTeachers();
+    
+    public void ImportStudents();
+    public void ExportStudents();
+}
