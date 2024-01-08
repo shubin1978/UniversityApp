eb6a49e2edb6bf14e2a36c5edfb2c538f2154ad3
yury <shubin1978@omnicommdv.ru>
2024-01-08 14:02:44 +0800

    Создание ICrud.cs

---
+++ DataAccessLevel/ICrud.cs
@@ -0,0 +1,10 @@
+﻿namespace UniversityApp.DataAccessLevel;
+
+public interface ICrud<T>
+{
+    public bool Insert(T obj);
+    public bool Update(T obj);
+    public bool Delete(T obj);
+    public IEnumerable<T>? GetAll();
+    public T? GetById(int id);
+}
