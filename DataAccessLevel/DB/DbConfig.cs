9d6d70238a716dc208a98e9db67f0407046da8b6
yury <shubin1978@omnicommdv.ru>
2024-01-08 14:03:12 +0800

    Создание DbConfig.cs

---
+++ DataAccessLevel/DB/DbConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace UniversityApp.DataAccessLevel.DB;
+
+public class DbConfig
+{
+    public string Host { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Database { get; set; }
+
+    public override string ToString()
+    {
+        return $"Host={Host};Username={Username};Password={Password};Database={Database}";
+    }
+
+    public static DbConfig? Import(string path = "db_config.json")
+    {
+        var json = File.ReadAllText(path);
+        return JsonSerializer.Deserialize<DbConfig>(json);
+    }
+}
